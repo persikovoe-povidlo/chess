302545d19e2b5c770372976af46a4e4e596872b6
persikovoe_povidlo <persikovoe.povidlo@gmail.com>
2025-04-09 04:02:37 +0300

    now randomly distributes pieces when the game first starts, on rematch just swaps pieces

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 from threading import Thread
+from random import randint
 
 HOST = '127.0.0.1'
 PORT = 25565
@@ -11,24 +12,33 @@ class Server:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((host, port))
         self.socket.listen()
-        self.player_w = None
-        self.player_b = None
-        self.active_player = self.player_w
+        self.player1 = None
+        self.player2 = None
+        self.active_player = self.player1
         self.socket = socket.socket()
         self.socket.bind(('127.0.0.1', 25565))
-        self.socket.listen(2)
+        self.socket.listen()
 
     def send_to_players(self, command):
-        self.player_w.send(command.encode())
-        self.player_b.send(command.encode())
+        self.player1.send(command.encode())
+        self.player2.send(command.encode())
 
     def get_connections(self):
         while self.running:
             connection, addr = self.socket.accept()
-            if self.player_w is None:
-                self.player_w = connection
-            elif self.player_b is None:
-                self.player_b = connection
+            if self.player1 is None and self.player2 is None:
+                if randint(0, 1):
+                    self.player1 = connection
+                else:
+                    self.player2 = connection
+            elif self.player1 is None:
+                self.player1 = connection
+            elif self.player2 is None:
+                self.player2 = connection
+            else:
+                connection.send('disconnect'.encode())
+                connection.close()
+                return
             Thread(target=self.listen, args=(connection,)).start()
 
     def listen(self, connection):
@@ -36,22 +46,23 @@ class Server:
             while self.running:
                 data = connection.recv(10).decode()
                 if data == 'disconnect':
-                    if connection == self.player_w:
-                        self.player_w = None
-                        if self.player_b:
-                            self.player_b.send('disconnect'.encode())
-                        self.player_b = None
-                    elif connection == self.player_b:
-                        if self.player_w:
-                            self.player_w.send('disconnect'.encode())
-                        self.player_w = None
-                        self.player_b = None
+                    if connection == self.player1:
+                        self.player1 = None
+                        if self.player2:
+                            self.player2.send('disconnect'.encode())
+                        self.player2 = None
+                    elif connection == self.player2:
+                        if self.player1:
+                            self.player1.send('disconnect'.encode())
+                        self.player1 = None
+                        self.player2 = None
                     connection.close()
 
                 elif data == 'start_game':
-                    if self.player_w and self.player_b:
-                        self.player_w.send('start w'.encode())
-                        self.player_b.send('start b'.encode())
+                    self.player1, self.player2 = self.player2, self.player1
+                    if self.player1 and self.player2:
+                        self.player1.send('start w'.encode())
+                        self.player2.send('start b'.encode())
 
                 elif data[0] == 'm':
                     self.send_to_players(data)
@@ -62,6 +73,7 @@ class Server:
                 elif data == 'undo':
                     self.send_to_players(data)
         except:
+            connection.close()
             print('connection to client lost')
 
     def stop(self):
